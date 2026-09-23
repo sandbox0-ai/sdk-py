92508b23e9faf1a240e84a5eef2abf355b450b75
Zuch Huang <zhihao.huang.1997@gmail.com>
2026-09-23 14:41:58 +0800

    Limit generated Python API to privileged sandbox class (#80)
    Co-authored-by: Zuch Huang <langyi.huang.1997@gmail.com>

--- sandbox0/apispec/models/container_spec_security_class.py
+++ sandbox0/apispec/models/container_spec_security_class.py
@@ -3,7 +3,6 @@ from enum import Enum
 
 class ContainerSpecSecurityClass(str, Enum):
     PRIVILEGED = "privileged"
-    STANDARD = "standard"
 
     def __str__(self) -> str:
         return str(self.value)
